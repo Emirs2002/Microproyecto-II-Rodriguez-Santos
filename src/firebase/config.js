6d9e55a3820d10565d2a76430f806978cbbc5edd
Emily <em.rodriguez@correo.unimet.edu.ve>
2023-03-11 18:37:04 -0400

    firebase connection

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
-
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -8,8 +10,17 @@ const firebaseConfig = {
   projectId: "cinema-show-71e2c",
   storageBucket: "cinema-show-71e2c.appspot.com",
   messagingSenderId: "485926470560",
-  appId: "1:485926470560:web:46fee04b90a0eda5d19ac5"
+  appId: "1:485926470560:web:46fee04b90a0eda5d19ac5",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app); //conexion con el modulo de la bd
+
+export const storage = getStorage(app);
+
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" }); //permite al usuario seleccionar entre sus cuentas de google
